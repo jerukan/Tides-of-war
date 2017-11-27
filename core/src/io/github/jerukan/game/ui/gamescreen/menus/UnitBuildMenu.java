29c62f4288ee67b60534b1accb3db2cc8b5b5cc7
jerukan <jerry21yan@gmail.com>
2017-11-26 20:32:59 -0800

    Polished the unit build menu
    Now it doesn't show up when you change a tile while still on the build menu.
    Also only shows units that you can build now.

--- core/src/io/github/jerukan/game/ui/gamescreen/menus/UnitBuildMenu.java
+++ core/src/io/github/jerukan/game/ui/gamescreen/menus/UnitBuildMenu.java
@@ -10,35 +10,51 @@ import io.github.jerukan.game.ui.ButtonGroup;
 import io.github.jerukan.game.ui.gamescreen.buttons.UnitBuildButton;
 import io.github.jerukan.util.BooleanFlag;
 import io.github.jerukan.util.Constants;
+import io.github.jerukan.util.Position;
 
 /** Menu that appears when a player is selecting a unit to build */
 
 public class UnitBuildMenu extends ButtonGroup {
 
+    private Position currentPos;
+
     public UnitBuildMenu(BooleanFlag[] flags) {
         super(flags);
-        //TODO make it automatically switch to new columns
+        currentPos = new Position();
         table.align(Align.bottomLeft);
+        resetTable();
+    }
+
+    public void resetTable() {
+        table.clear();
         for(BaseUnit unit : AllUnits.list) {
-            table.add(new UnitBuildButton(unit, flagFromArray("build"))).pad(5).row();
+            if(unit.canBuild(GameState.instance.boardManager.getSelectedPosition(), GameState.instance.getCurrentPlayer())) {
+                table.add(new UnitBuildButton(unit, flagFromArray("build"))).pad(5).row();
+            }
         }
     }
 
     @Override
     public void updateVisibility() {
+        if(!currentPos.equals(GameState.instance.boardManager.getSelectedPosition())) {
+            flagFromArray("build").setState(false);
+            currentPos.setPos(GameState.instance.boardManager.getSelectedPosition());
+        }
         if(flagFromArray("build").getState()) {
             if(GameState.instance.boardManager.getSelectedPosition().isValid()) {
                 if (GameState.instance.unitManager.positionAvailable(GameState.instance.boardManager.getSelectedPosition())) {
                     WorldRenderer.boardCam.updateOffsets();
                     Sprite s = GameState.instance.boardManager.tileFromPosition(GameState.instance.boardManager.getSelectedPosition()).getSprite();
-                table.setPosition(s.getX() + s.getWidth() + Constants.TILE_MENU_OFFSET - WorldRenderer.boardCam.camOffsetX, s.getY() - WorldRenderer.boardCam.camOffsetY);
+                    table.setPosition(s.getX() + s.getWidth() + Constants.TILE_MENU_OFFSET - WorldRenderer.boardCam.camOffsetX, s.getY() - WorldRenderer.boardCam.camOffsetY);
                     table.setVisible(true);
-                } else {
+                }
+                else {
                     flagFromArray("build").setState(false);
                 }
             }
         }
         else {
+            resetTable();
             table.setVisible(false);
         }
     }
